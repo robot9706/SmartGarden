58209785dd2c73f4864628e6282ce20172600d76
Kalotai Bence <kalotaibence@gmail.com>
2020-05-10 14:55:21 +0200

    add angular skeleton

---
+++ client/SmartGarden/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { GardenComponent } from './garden/garden.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GardenComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
